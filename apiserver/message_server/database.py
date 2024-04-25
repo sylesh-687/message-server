d04fa3c797dc4308cb14307611a86008c464adc7
Shailesh Thakur <sylesh687@gmail.com>
2024-04-25 22:18:17 +0530

    Cleaned UP

--- apiserver/message_server/database.py
+++ apiserver/message_server/database.py
@@ -1,11 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
-# SQLALCHEMY_DATABASE_URL = 'sqlite:///./messages.db'
-
-SQLALCHEMY_DATABASE_URL = 'postgresql://myuser:Welcome123@192.168.64.41/messages'
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+database_url=os.getenv("SQLALCHEMY_DATABASE_URL")
+engine = create_engine(database_url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
