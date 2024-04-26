c83778629c21341de39e1b8953138b334f198f07
Shailesh Thakur <sylesh687@gmail.com>
2024-04-26 09:16:42 +0530

    Added TF for Kubernetes Cluster eks

--- apiserver/terraform/modules/eks/variables.tf
+++ apiserver/terraform/modules/eks/variables.tf
@@ -13,3 +13,16 @@ variable "aws_region" {
   description = "AWS region to deploy the EKS cluster"
   nullable    = false
 }
+variable "PROVIDER_ACCESS_KEY" {
+  type        = string
+  description = "Access key for AWS provider. This has to be passed via environment variable TF_VAR_PROVIDER_ACCESS_KEY"
+  nullable    = false
+  sensitive   = true
+}
+
+variable "PROVIDER_SECRET_KEY" {
+  type        = string
+  description = "Secret key for AWS provider. This has to be passed via environment variable TF_VAR_PROVIDER_SECRET_KEY"
+  nullable    = false
+  sensitive   = true
+}
